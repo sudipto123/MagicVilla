7f27cfed54c293be72c1be2a9b81e31b757776c7
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-03-07 14:36:58 +0530

    17. Villa Data Store

--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -3,7 +3,6 @@
     public class VillaDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public string Name { get; set; }        
     }
 }
