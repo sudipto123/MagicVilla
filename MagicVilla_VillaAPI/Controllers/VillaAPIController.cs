77aaf621e4697faa9218037df2a629b91567bc1b
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-03-07 14:45:22 +0530

    18. Get individual Villa

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -13,7 +13,12 @@ namespace MagicVilla_VillaAPI.Controllers
         [HttpGet]
         public IEnumerable<VillaDTO> GetVillas()
         {
-            return VillaStore.villaList;
+            return VillaStore.villaList; 
+        }
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
         }
     }
 }
