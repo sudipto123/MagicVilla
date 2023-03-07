28bf7838f310d145ad9c948de3cc4b62b4f17b38
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-03-07 22:18:38 +0530

    22. CreatedAtRoute

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -16,7 +16,7 @@ namespace MagicVilla_VillaAPI.Controllers
         {
             return Ok(VillaStore.villaList); 
         }
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetVilla")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -37,9 +37,9 @@ namespace MagicVilla_VillaAPI.Controllers
             return Ok(villa);
         }
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
         {
             if (villaDTO == null)
@@ -52,7 +52,7 @@ namespace MagicVilla_VillaAPI.Controllers
             }
             villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
             VillaStore.villaList.Add(villaDTO);
-            return Ok(villaDTO);
+            return CreatedAtRoute("GetVilla", new { id = villaDTO.Id } ,villaDTO);
         }
     }
 }
--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -6,8 +6,8 @@ namespace MagicVilla_VillaAPI.Data
     {
         public static List<VillaDTO> villaList = new List<VillaDTO>()
         {
-            new VillaDTO {Id = 1, Name = "Pool View"},
-            new VillaDTO {Id = 2, Name= "Beach View"}
+            new VillaDTO {Id = 1, Name = "Sohini Villa"},
+            new VillaDTO {Id = 2, Name= "Sudipto Villa"}
         };
     }        
 }
