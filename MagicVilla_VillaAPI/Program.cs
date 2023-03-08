e64bd3d5d6e28abe1a9139bbc9d9632acfd915d6
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-03-08 14:10:05 +0530

    38. Use Connection String

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,5 +1,8 @@
 //using MagicVilla_VillaAPI.Logging;
 
+using MagicVilla_VillaAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 //Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
 //builder.Host.UseSerilog();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
 builder.Services.AddControllers(option =>
 {
     /* Does not provide output in text/plan but provides output in JSON and XML */
