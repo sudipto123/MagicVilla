84fef6111afb45b7e9db2af41a3ce182b515ecbe
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-03-08 14:00:45 +0530

    37. Connection String and DbSet Entity

--- MagicVilla_VillaAPI/Models/Villa.cs
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -1,7 +1,12 @@
-﻿namespace MagicVilla_VillaAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicVilla_VillaAPI.Models
 {
     public class Villa
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Details { get; set; }
