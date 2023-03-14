103d238cf4f950933a33983766fee0b6d41566f7
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-03-14 14:36:19 +0530

    Revert "Play"
    This reverts commit 820e80422055e1d457c9767e9fde0823f889fe83.

--- MagicVilla_VillaAPI/Models/APIResponse.cs
+++ MagicVilla_VillaAPI/Models/APIResponse.cs
@@ -4,10 +4,6 @@ namespace MagicVilla_VillaAPI.Models
 {
     public class APIResponse
     {
-        public APIResponse()
-        {
-            ErrorMessages = new List<string>();
-        }
         public HttpStatusCode StatusCode { get; set; }
         public bool IsSuccess { get; set; } = true;
         public List<string> ErrorMessages { get; set; }
