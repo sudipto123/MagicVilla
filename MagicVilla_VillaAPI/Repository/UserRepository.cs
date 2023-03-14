0bc7a63d5cb989dbf35f586904b4d86e77ab7c04
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2023-03-14 11:37:17 +0530

    87. Implement User Repository - Register

--- MagicVilla_VillaAPI/Repository/UserRepository.cs
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -1,6 +1,45 @@
-﻿namespace MagicVilla_VillaAPI.Repository
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepository;
+
+namespace MagicVilla_VillaAPI.Repository
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        private readonly ApplicationDbContext _db;
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool IsUniqueUser(string username)
+        {
+            var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+            if (user == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
+        {
+            LocalUser user = new()
+            {
+                UserName = registrationRequestDTO.UserName,
+                Password = registrationRequestDTO.Password,
+                Name = registrationRequestDTO.Name,
+                Role = registrationRequestDTO.Role
+            };
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
+        }
     }
 }
